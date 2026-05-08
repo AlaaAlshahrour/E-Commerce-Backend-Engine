e3185c579e7c043ed00a6930d859fd35e15accd8
C0de871 <muhammadaydi7@gmail.com>
2026-05-08 21:58:57 +0300

    add rate limiter for most api's

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
 use Laravel\Sanctum\PersonalAccessToken;
 use Laravel\Sanctum\Sanctum;
 
@@ -22,5 +25,59 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+
+        RateLimiter::for('public-api', function (Request $request) {
+
+            return Limit::perMinute(120)
+                ->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for('authenticated-api', function (Request $request) {
+
+        return Limit::perMinute(120)
+            ->by($request->user()->id);
+        });
+
+        RateLimiter::for('login', function (Request $request) {
+
+            return Limit::perMinute(5)
+                ->by($request->email . $request->ip());
+        });
+
+        RateLimiter::for('register', function (Request $request) {
+
+            return Limit::perMinute(3)
+                ->by($request->ip());
+        });
+
+        RateLimiter::for('cart', function (Request $request) {
+
+            return Limit::perMinute(60)
+                ->by($request->user()->id);
+        });
+
+        RateLimiter::for('checkout', function (Request $request) {
+
+            return Limit::perMinute(5)
+                ->by($request->user()->id);
+        });
+
+        RateLimiter::for('wallet', function (Request $request) {
+
+            return Limit::perMinute(3)
+                ->by($request->user()->id);
+        });
+
+        RateLimiter::for('inventory-update', function (Request $request) {
+
+            return Limit::perMinute(20)
+                ->by($request->user()->id);
+        });
+
+        RateLimiter::for('admin-actions', function (Request $request) {
+
+            return Limit::perMinute(10)
+                ->by($request->user()->id);
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,8 @@ use Illuminate\Support\Facades\Route;
 
 ////////////   Auth   /////////////////////
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:register')->name('register');
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:login')->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -21,11 +21,15 @@ Route::middleware('auth:sanctum')->group(function () {
 
 ////////   Product & Category   //////////////////
 
-Route::apiResource('products', ProductController::class)
-    ->except(['store', 'update', 'destroy']);
 
-Route::apiResource('categories', CategoryController::class)
-    ->except(['store', 'update', 'destroy']);
+Route::middleware('throttle:public-api')->group(function () {
+    Route::apiResource('products', ProductController::class)
+        ->except(['store', 'update', 'destroy']);
+
+    Route::apiResource('categories', CategoryController::class)
+        ->except(['store', 'update', 'destroy']);
+});
+
 
 Route::middleware('auth:sanctum')->group(function () {
 
@@ -38,7 +42,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 ////////   cart   //////////////////
 
-Route::middleware('auth:sanctum')->prefix('cart')->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:cart'])->prefix('cart')->group(function () {
     Route::post('/add/{product_id}',  [CartController::class, 'add']);
     Route::get('/',                   [CartController::class, 'getCartProducts']);
     Route::delete('/clear',           [CartController::class, 'deleteAll']);
@@ -47,7 +51,7 @@ Route::middleware('auth:sanctum')->prefix('cart')->group(function () {
 });
 
 ////////   inventory   //////////////////
-Route::middleware('auth:sanctum')->prefix('inventory')->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:inventory-update'])->prefix('inventory')->group(function () {
     Route::get('/',                [InventoryController::class, 'index']);
     Route::get('/{productId}',     [InventoryController::class, 'show']);
     Route::put('/{productId}',     [InventoryController::class, 'update']);
@@ -55,14 +59,17 @@ Route::middleware('auth:sanctum')->prefix('inventory')->group(function () {
 
 ////////   orders   //////////////////
 Route::middleware('auth:sanctum')->prefix('orders')->group(function () {
-    Route::get('/',                [OrderController::class, 'index']);
-    Route::post('/checkout',               [OrderController::class, 'checkout']); // Create order + Process payment
-    Route::get('/{order}',         [OrderController::class, 'show']);
-    Route::put('/{id}/status',     [OrderController::class, 'updateStatus'])->middleware('role:Admin');
+    Route::get('/',                [OrderController::class, 'index'])->middleware('throttle:authenticated-api');
+    Route::post('/checkout',               [OrderController::class, 'checkout'])->middleware('throttle:checkout');
+    Route::get('/{order}',         [OrderController::class, 'show'])->middleware('throttle:authenticated-api');
+    Route::put('/{id}/status',     [OrderController::class, 'updateStatus'])->middleware([
+            'role:Admin',
+            'throttle:admin-actions'
+        ]);
 });
 
 ////////   wallet   //////////////////
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/wallet', [WalletController::class, 'show']);
-    Route::post('/wallet/topup', [WalletController::class, 'topUp']);
+    Route::get('/wallet', [WalletController::class, 'show'])->middleware('throttle:authenticated-api');
+    Route::post('/wallet/topup', [WalletController::class, 'topUp'])->middleware('throttle:wallet');
 });
